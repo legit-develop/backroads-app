96b1bdd42481b68a8bb7b65923f37660fda3db8f
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-02-07 13:31:23 +0200

    first commit

---
+++ src/components/Services.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Title from "./Title";
+import { Services } from "../data";
+
+const services = () => {
+  return (
+    <section className="section services" id="services">
+      <Title title="our" subTitle="services" />
+      <div className="section-center services-center">
+        {Services.map((service) => {
+          const { icon, title, text } = service;
+          return (
+            <article className="service">
+              <span className="service-icon">
+                <i className={icon}></i>
+              </span>
+              <div className="service-info">
+                <h4 className="service-title">{title}</h4>
+                <p className="service-text">{text}</p>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default services;
